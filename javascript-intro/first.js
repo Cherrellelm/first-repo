483daf0fce554e24b1f7e9a8de273fbc89954abc
Cherrellelm <cherrelle.l.m@gmail.com>
2023-01-14 20:21:27 -0600

    created new files and practiced js

---
+++ javascript-intro/first.js
@@ -0,0 +1,15 @@
+//declaring variables
+let intRate;
+let firstName;
+let customerWebsite
+let phoneNumber;
+
+
+intRate = 10;
+firstName = "Cherrelle";
+customerWebsite = "wwww.cherrelle.com";
+phoneNumber = "(404)555-5555"
+
+console.log("firstName")
+
+alert("Hello" + firstName)
