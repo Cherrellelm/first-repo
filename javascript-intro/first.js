e0aac3b931b4b26c3562900b301a174b7ed990a2
Cherrellelm <cherrelle.l.m@gmail.com>
2023-01-17 18:25:34 -0600

    javascript exercises

--- javascript-intro/first.js
+++ javascript-intro/first.js
@@ -3,7 +3,7 @@ let intRate;
 let firstName;
 let customerWebsite
 let phoneNumber;
-
+const fName = 'Cherrelle';
 
 intRate = 10;
 firstName = "Cherrelle";
@@ -12,4 +12,7 @@ phoneNumber = "(404)555-5555"
 
 console.log("firstName")
 
-alert("Hello" + firstName)
+//alert("Hello" + firstName)
+
+const greeting = `Hello, ${fName}`;
+console.log(greeting)
