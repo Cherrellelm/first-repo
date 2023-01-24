3d30a14b6cef1e1e2487a95b5bd89a0fa669b49a
Cherrellelm <cherrelle.l.m@gmail.com>
2023-01-23 23:41:54 -0600

    more function practice

--- javascript-intro/jan23.js
+++ javascript-intro/jan23.js
@@ -1,14 +1,13 @@
 /*Convert age in years to age in days*/
 
-let ageInYears;
+let ageInYears = 65;
 let ageInDays;
-
-ageInYears = 65;
-
-console.log(`If someone is ${ageInYears}, they are ${ageInDays} days old`);
 
 ageInDays = convertAgeToDays ();
 
+console.log('The age in days is: '+ ageInDays );
+
 function convertAgeToDays (){
+    
 return ageInYears * 365;
-};
+}
