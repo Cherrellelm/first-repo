ed7455c3e206013ab9dde1b1a1bc3e4a6d1818ab
Cherrellelm <cherrelle.l.m@gmail.com>
2023-01-27 20:02:33 -0600

    completed puppy age exercise

--- javascript-intro/jan27.js
+++ javascript-intro/jan27.js
@@ -10,3 +10,13 @@ function tellFortune(children, partner, lLocation, job) {
 }
 
 tellFortune(numberOfChildren, partnerName, locations, jobTitle);
+
+// The Puppy Age Calculator
+
+function calculateDogAge (puppyAge) {
+console.log ('Your doggie is ' + (puppyAge * 7) + ' years old in dog years!')
+}
+
+calculateDogAge(2);
+calculateDogAge(5);
+calculateDogAge(11);
