cb151578e939a4efb295a2642e4dfbfe74457889
Cherrellelm <cherrelle.l.m@gmail.com>
2023-01-31 08:28:00 -0600

    more functions on jan27.js

--- javascript-intro/jan27.js
+++ javascript-intro/jan27.js
@@ -31,3 +31,27 @@ function calculateSupply(age, amount) {
     console.log ('You will need ' + supply + ' to last you until the ripe old age of ' + age + '.')
 }
 calculateSupply(75,3);
+
+//random function practice
+
+function addNumbers(num1, num2) {
+    console.log (num1 + num2);
+}
+
+addNumbers(54, 79);
+addNumbers(60, 25);
+addNumbers(85, 236);
+
+//if store has pink pants
+
+let pinkPants = false;
+
+function storeStock() {
+    if (pinkPants) {
+        console.log('We have the pants you want, come buy them!');
+    }else {
+        console.log ('Sorry, we only have blue pants.');
+    }
+}
+
+storeStock();
