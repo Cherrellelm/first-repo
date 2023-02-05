264f1f7542a91ac069d1bb295c49acf27c82125f
Cherrellelm <cherrelle.l.m@gmail.com>
2023-02-05 17:49:16 -0600

    fizzbuzz problem, loops

---
+++ javascript-intro/feb5.js
@@ -0,0 +1,8 @@
+//Write a program that displays the num ber of times each number is rolled in a die
+//Initialize a counter and include the check condition with an increment
+
+console.log ('Completed rolling the die 100 times !!!');
+
+for (let counter = 1; counter <=100; counter = counter + 1) {
+    console.log (`The outcome of ONE occurred ${counter} times.`);
+}
---
+++ javascript-intro/fizzbuzz.js
@@ -0,0 +1,13 @@
+//start at 1
+
+// end at pre-determined value
+
+//multiple of 3, print Fizz
+
+//multiple of 5, print Buzz
+
+//multiple of 3 AND 5. print FizzBuzz
+
+// output to console log
+
+//Need to print numbers that are NOT multiple of 3 OR 5
