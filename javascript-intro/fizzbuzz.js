66895c2dc748a1479e92bbb70113264f3a52274c
Cherrellelm <cherrelle.l.m@gmail.com>
2023-02-05 18:19:57 -0600

    completed fizzbuzz exercise

--- javascript-intro/fizzbuzz.js
+++ javascript-intro/fizzbuzz.js
@@ -2,12 +2,39 @@
 
 // end at pre-determined value
 
+let endingNumber = 47;
+
+for(let counter = 1; counter <= endingNumber; counter++) {
+    // output to console log
+    //flag to check divisability by 3
+    let isDivisibleByThree = false;
+    let isDivisibleByFive = false;
+    
+    if(counter % 3 ==0) {
+        isDivisibleByThree = true;
+    }
+//check divisability by 5
+    if (counter % 5 ==0) {
+        isDivisibleByFive = true;
+    }
+    if (isDivisibleByThree && isDivisibleByFive) {
+        console.log ("FizzBuzz")
+    }else if (isDivisibleByThree == true){
+        console.log ("Fizz");
+    } else if (isDivisibleByFive ==true){
+        console.log ("Buzz");
+    }else {
+        console.log(counter);
+    }
+
+}
+
 //multiple of 3, print Fizz
 
 //multiple of 5, print Buzz
 
 //multiple of 3 AND 5. print FizzBuzz
 
-// output to console log
+
 
 //Need to print numbers that are NOT multiple of 3 OR 5
