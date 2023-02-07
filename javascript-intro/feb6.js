798e05d477129888ebedac85aab06767f9547a69
Cherrellelm <cherrelle.l.m@gmail.com>
2023-02-06 22:53:59 -0600

    loop practice

---
+++ javascript-intro/feb6.js
@@ -0,0 +1,17 @@
+//#1 — Print the numbers from 0 to 15
+/*
+for (let counter = 10; counter >=-20; counter--) {
+    if (counter % 2 == 0){
+        console.log(counter);
+    }
+    
+}
+*/
+//Print the numbers 50 down to 20 in descending order, but only if the numbers are multiples of 3.
+
+for (let i = 50; i >=20; i--) {
+    if (i % 3==0) {
+     console.log(i);
+    }
+    
+}
