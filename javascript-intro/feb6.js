3d55cda2e415bd87a8e93808b35952220d311024
Cherrellelm <cherrelle.l.m@gmail.com>
2023-02-09 22:27:10 -0600

    loops practice '

--- javascript-intro/feb6.js
+++ javascript-intro/feb6.js
@@ -8,10 +8,33 @@ for (let counter = 10; counter >=-20; counter--) {
 }
 */
 //Print the numbers 50 down to 20 in descending order, but only if the numbers are multiples of 3.
-
+/*
 for (let i = 50; i >=20; i--) {
     if (i % 3==0) {
      console.log(i);
     }
     
 }
+*/
+// while loop practice
+//Prompt the user to enter the starting fuel level. The loop should continue until the user enters a positive 
+//value greater than 5,000 but less than 30,000.
+
+let startingFuel = 6000;
+let astronauts;
+let altitude;
+
+while (startingFuel >= 0) {
+    if (startingFuel >= 5000 && startingFuel <= 30000){
+        startingFuel = startingFuel + 1000;
+    console.log(startingFuel); 
+}
+}
+/*
+let startingFuel = 1000;
+
+
+for (startingFuel = < 5000; startingFuel >=5000 && startingFuel <=30000; startingFuel = startingFuel + 1000) {
+    console.log(startingFuel);
+}
+*/
