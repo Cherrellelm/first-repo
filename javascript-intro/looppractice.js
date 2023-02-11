6f6efb4ddf55f52e404c1091caae544a9a793d8b
Cherrellelm <cherrelle.l.m@gmail.com>
2023-02-11 16:15:01 -0600

    completed avantutor exercise 6

--- javascript-intro/looppractice.js
+++ javascript-intro/looppractice.js
@@ -9,5 +9,16 @@ for (i = 0; i <= str1.length; i++) {
     } else {
         console.log(str1.charAt(i));
     }
-    
+}
+
+//Exercise 6 - Check if a string contains the letter “y”. Print “yes” if it does and “no” if it does not.
+
+let str2 = "don't know why";
+
+for ( i = 0; i <= str2.length; i++) {
+    if(str2.match("y")){
+        console.log('yes');
+    }else {
+        console.log('no');
+    }   
 }
