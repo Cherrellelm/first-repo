b433a1feebf584a2399a64cb7162510a10e40cd3
Cherrellelm <cherrelle.l.m@gmail.com>
2023-02-11 14:46:28 -0600

    avantutor loop exercises

---
+++ javascript-intro/looppractice.js
@@ -0,0 +1,8 @@
+//https://avantutor.com/blog/10-simple-javascript-for-loop-exercises/
+//Exedrcise 5 - Given a string change the every second letter to an uppercase ‘Z’. Assume there are no space.
+
+let str1 = "javascript";
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i)
+}
