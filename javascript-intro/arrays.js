d71ec9dd49d536e9927dfa5db158e4165f3eaecc
Cherrellelm <cherrelle.l.m@gmail.com>
2023-02-12 13:45:11 -0600

    array methods practice on arrays.js

--- javascript-intro/arrays.js
+++ javascript-intro/arrays.js
@@ -4,6 +4,27 @@ let prices = [10, 25, 300];
 let countries = ["Japan", "Ecuador", "Mexico", "Portugal", "Ghana"];
 let capitals = ["Tokyo", "Quito", "Mexico City", "Lisbon", "Accra"];
 
+console.log(countries.join(" ^ "));
+
+countries.pop()
+console.log(countries.join(" ^ "));
+
+countries.pop()
+console.log(countries.join(" ^ "));
+
+countries.pop()
+console.log(countries.join(" ^ "));
+
+countries.pop()
+console.log(countries.join(" ^ "));
+
+//array methods
+console.log(Array.isArray(fruits));
+// push() adds an element to an array and returns the length pop() removes the last elements and returns the removed element
+//shift() removes the first element and shifts all elements down on the index
+countries.push("India");
+capitals.push("New Delhi");
+
 console.log(employees);
 console.log(typeof(employees));
 console.log(employees[0]);
@@ -23,3 +44,7 @@ for (let counter = 0; counter < countries.length; counter++) {
 for (let counter = 0; counter < countries.length; counter++) {
     console.log(`${capitals[counter]} is the capital of ${countries[counter]}`);
 }
+
+console.log(fruits.toString());
+console.log(fruits.join(" :) ")); //- what you put in the () will go in between array elements
+
