a7a12eff7652e980f64986911887f7acc8b63e62
Cherrellelm <cherrelle.l.m@gmail.com>
2023-03-03 21:34:20 -0600

    completed more arrow functions on arrowfunctions0303.js

--- javascript-intro/arrowfunctions0303.js
+++ javascript-intro/arrowfunctions0303.js
@@ -1,3 +1,4 @@
+//first variation
 let total;
 
 total = sum(10,20); 
@@ -8,8 +9,42 @@ function sum(first, second) {
     return first + second;
 }
 
+//on line 13 you could remove the curly braces and let total2 = (first, second) => first + second;
+//if you have multiple lines you have to use the curly braces
+//Second variation
 let total2 = (first, second) => {
     return first + second;
 }
 
 console.log(total2(50,60)); 
+
+//third variation
+let total3 = (first, second) => first + second;
+
+console.log(total3(200,400));
+
+//if there is one paremeter, you don't need ()
+
+//let greetings2 = language => =0
+
+//greetings into an arrow function
+
+let greetings = (language, studentName) => {
+    if(language == "HAWAII") {
+        return "Aloha " + studentName;
+    } else if(language == "SPANISH") {
+        return "Hola " + studentName;
+    }else {
+        return "Hello " + studentName;
+    }
+}
+
+console.log(greetings ("SPANISH", "Cherrelle"));
+console.log(greetings ("HAWAII", "Kat"));
+console.log(greetings ("HINDI", "JayW"));
+
+let meow = studentName2 => "Namaste " + studentName2;
+
+console.log(meow("Cherrelle"));
+console.log(meow("Kat"));
+console.log(meow("JayW"));
