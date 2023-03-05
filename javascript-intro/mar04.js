71832ca21c72ce701146921ad9a56025ffc57b4c
Cherrellelm <cherrelle.l.m@gmail.com>
2023-03-04 22:20:15 -0600

    added tax calculation to mar04.js

--- javascript-intro/mar04.js
+++ javascript-intro/mar04.js
@@ -22,23 +22,33 @@ let arrStateTax = [4, 6, 4, 4.75, 6, 7, 6.25];
 
 let btn = document.querySelector("#submitBtn");
 let stateName = document.querySelector("#statename");
-let outputDiv = document.querySelector('#output');
+let outputDiv = document.querySelector("#output");
+let orderTotal = document.querySelector("#orderTotal");
 
 let outputLine;
 loadStates();
 
-btn.addEventListener('click', () => displayStateName());
+
+btn.addEventListener('click', () => displayTaxCalculation());
 
 btn.addEventListener('mouseover', () => console.log("UFO is hovering over us!!!"));
 
-function displayStateName() {
+function displayTaxCalculation() {
+
+    let taxRate = getStateTaxRate(stateName.value);
+    let taxAmount = calcTax(Number(orderTotal.value), taxRate);
+    let totalBill = Number(orderTotal.value) + taxAmount;
 
     if (!document.getElementById("outputMessage")) {
         outputLine = document.createElement('p');
         outputLine.setAttribute('id', 'outputMessage');
 }
 
-    outputLine.textContent = statename.value;
+//Thank you for choosing EATS in Georgia. Your bill is $$$$.
+
+    outputLine.textContent = "Thank you for choosing EATS in " + stateName.value + ". " 
+        + "Your bill is " + totalBill;
+
     outputDiv.appendChild(outputLine);
 }
 
@@ -52,3 +62,17 @@ function loadStates() {
     }
     }
 
+let getStateTaxRate = (stateAbbr) => {
+
+    //search the array for the corresponding tax rate
+    for(let counter = 0; counter < arrStateAbbr.length; counter++) {
+        if(stateAbbr == arrStateAbbr[counter]) {
+                //return the tax rate
+            return arrStateTax[counter];
+        }
+    }
+
+}
+
+let calcTax = (amount, taxRate) => (amount * (taxRate/100));
+    //return the tax amount
