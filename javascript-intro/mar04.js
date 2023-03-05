b2eddaefbf3877eee57aaa8f0fadf0a4909d5853
Cherrellelm <cherrelle.l.m@gmail.com>
2023-03-04 21:20:08 -0600

    created loadStates function on line 46

--- javascript-intro/mar04.js
+++ javascript-intro/mar04.js
@@ -1,17 +1,35 @@
+let arrStateName = [
+    "Alabama",
+    "Florida",
+    "Georgia",
+    "North Carolina",
+    "South Carolina",
+    "Tennessee",
+    "Texas",
+];
+
+let arrStateAbbr = [
+    "AL",
+    "FL",
+    "GA",
+    "NC",
+    "SC",
+    "TN",
+    "TX"
+];
+
+let arrStateTax = [4, 6, 4, 4.75, 6, 7, 6.25];
+
 let btn = document.querySelector("#submitBtn");
 let stateName = document.querySelector("#statename");
 let outputDiv = document.querySelector('#output');
 
 let outputLine;
+loadStates();
 
 btn.addEventListener('click', () => displayStateName());
 
 btn.addEventListener('mouseover', () => console.log("UFO is hovering over us!!!"));
-
-let stateOption = document.createElement("option");
-stateOption.text = "South Carolina";
-stateOption.value = "SC";
-stateName.appendChild(stateOption);
 
 function displayStateName() {
 
@@ -23,3 +41,14 @@ function displayStateName() {
     outputLine.textContent = statename.value;
     outputDiv.appendChild(outputLine);
 }
+
+
+function loadStates() {
+    for (let counter = 0; counter < arrStateName.length; counter++) {
+        let stateOption = document.createElement("option");
+        stateOption.text = arrStateName[counter];
+        stateOption.value = arrStateAbbr[counter];
+        stateName.appendChild(stateOption);
+    }
+    }
+
