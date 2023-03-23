500c17580beb4c91b1b0295e43ed60aea4324c6a
Cherrellelm <cherrelle.l.m@gmail.com>
2023-03-22 21:35:13 -0500

    created for loop to loop through keys for values

--- javascript-intro/obj-menu.js
+++ javascript-intro/obj-menu.js
@@ -1,18 +1,22 @@
-//let menu = new Object();
-let menu = {
-    code: "CC",
-    item: "Chicken Chili",
-    price: 7,
-    isAvailable: true,
-    isVegan: false,
-    "Chefs Favorite": true,
-};
+function addMenuItem(code, item, price, isAvailable, isLocallySourced) {
+    return {
+        code,
+        item,
+        price,
+        isAvailable,
+        isLocallySourced
+    };
+}
+// you could delete the first key in code: code, for example and it will function the same way
+let menu1 = addMenuItem("CC", "Chicken Chili", 7, true, false);
 
-console.log(menu.item);
+let menu2 = addMenuItem("SO", "Soda", 1, false, true);
 
-let anyKey = "Chefs Favorite"; 
+console.log(menu1.price);
+console.log("price" in menu1); // boolean checking to see if the key exists, can use in an if condition
+console.log(menu2.price);
 
-menu[anyKey] = true;
-menu.anotherKey = true;
-
-console.log(menu.anotherKey)
+//use a for loop to loop through all keys in the menu1 object
+for(let key in menu1 ) {
+    console.log("For the " + key + ", the value is " + menu1[key]);
+}
