c4e9e7d4bb979d2ff34e509404d1be7999eafa53
Cherrellelm <cherrelle.l.m@gmail.com>
2023-03-25 10:40:41 -0500

    2d arrays practice

---
+++ javascript-intro/2d-arrays.js
@@ -0,0 +1,21 @@
+let menuItems = ["Chicken Chili","Jerk Tilapia","Black Beans","Sweet Potato","Homemade Brownie","Soda"];
+let menuCodes = ["CC","JT","BB","SP","HB","SO"];
+let menuPrices = [7, 7, 4, 4, 2, 1];
+
+
+let menu = [
+    ["CC", "Chicken Chili", 7],
+    ["JT", "Jerk Tilapia", 7],
+    ["BB", "Black Beans" , 4],
+    ["SP", "Sweet Potato", 4],
+    ["HB", "Homemade Brownie", 2],
+    ["SO", "Soda", 1]
+];
+
+console.log(menu);
+
+console.log(menuItems[2]);
+
+console.log(menu[2][1]);
+
+console.table(menu);
--- javascript-intro/obj-menu.js
+++ javascript-intro/obj-menu.js
@@ -20,13 +20,13 @@ let countryCodes = {
 let menu1 = addMenuItem("CC", "Chicken Chili", 7, true, false);
 menu1.prepare();
 
-/*let menu2 = addMenuItem("SO", "Soda", 1, false, true);
+let menu2 = addMenuItem("SO", "Soda", 1, false, true);
 
 console.log(menu1.price);
 console.log("price" in menu1); // boolean checking to see if the key exists, can use in an if condition
 console.log(menu2.price);
 
-//use a for loop to loop through all keys in the menu1 object
+/*//use a for loop to loop through all keys in the menu1 object
 for(let key in menu1 ) {
     console.log("For the " + key + ", the value is " + menu1[key]);
 }
@@ -45,4 +45,43 @@ let stateTax = {
     }
 }
 
-console.log(stateTax.calculateTax(500));
+console.log(stateTax.stateName);
+
+stateTax.stateName = "GEORGIA";
+
+console.log(stateTax.stateName);
+
+console.log(stateTax.calculateTax(1000));
+
+let user = {
+    name: "John",
+    surname: "Smith",
+}
+
+user.name = "pete";
+
+//delete user.name;
+
+console.log(user.name);
+
+function isEmpty(userOb) {
+    for(let key2 in user) {
+       console.log(false);
+    }
+    console.log(true);
+}
+isEmpty(user);
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  }
+
+  let sum = 0;
+
+  for(let key in salaries) {
+    sum += salaries[key];
+    console.log(sum);
+  }
+
